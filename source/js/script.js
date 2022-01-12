2b2f351eb561c532916a03cceb1956afe5bf9518
iamvlado93 <vladrjeousski@gmail.com>
2022-01-12 13:22:21 +0300

    feat: added fetching users

--- source/js/script.js
+++ source/js/script.js
@@ -1,1 +1,25 @@
-
+function fetchUsers() {
+  fetch("https://api.github.com/users")
+    .then((res) => {
+      if (!res.ok) {
+        throw Error("Error");
+      }
+      return res.json();
+    })
+    .then((data) => {
+      console.log(data);
+      const html = data
+        .map((user) => {
+          return `
+          <div class='user'>
+            <p>${user.login}</p>
+            <p><img src=${user.avatar_url} alt=${user.login}></p>
+          </div>
+          `;
+        })
+        .join("");
+      console.log(html);
+      document.querySelector("#users").insertAdjacentHTML("afterbegin", html);
+    });
+}
+fetchUsers();
